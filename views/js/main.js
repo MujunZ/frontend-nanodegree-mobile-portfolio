175c7bbb263bf828277dac211325add209ca3f86
MujunZ <o.6180339@gmail.com>
2017-01-04 15:37:25 -0500

    add ramdomPizzaContainer variable

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var ramdomPizzaContainer = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < ramdomPizzaContainer.length; i++) {
+      var dx = determineDx(ramdomPizzaContainer[i], size);
+      var newwidth = (ramdomPizzaContainer[i].offsetWidth + dx) + 'px';
+      ramdomPizzaContainer[i].style.width = newwidth;
     }
   }
 
