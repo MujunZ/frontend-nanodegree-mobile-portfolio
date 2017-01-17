e49cc0cc2bb07fde09ff7c13a4c133172b253910
MujunZ <o.6180339@gmail.com>
2017-01-17 10:48:51 -0500

    Chore: uglify JS, nano CSS, replace index.html
    Grunt uglify JS and nano CSS, but responsive_image can't run.
    
    Replace index.html etc. and going to optimize this page again.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,68 +1,4 @@
 module.exports = function(grunt) {
-
-// //var mozjpeg = require('imagemin-mozjpeg');
- 
-// grunt.initConfig({
-//   imagemin: {                          // Task 
-//     static: {                          // Target 
-//       options: {                       // Target options 
-//         optimizationLevel: 3,
-//         svgoPlugins: [{ removeViewBox: false }]
-//         //use: [mozjpeg()]
-//       },
-//       files: {                         // Dictionary of files 
-//         'views/images/reduced/pizza.png': 'views/images/pizza.png', // 'destination': 'source' 
-//         'views/images/reduced/pizzeria.jpg': 'views/images/pizzeria.jpg'
-//       }
-//     },
-//   }
-// });
- 
-// grunt.loadNpmTasks('grunt-contrib-imagemin');
-// grunt.registerTask('default', ['imagemin']);
-
-//grunt uglify
-  //Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'views/js/main.js',
-        dest: 'views/js/main.min.js'
-      }
-    }
-  });
-
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
-  // //grunt cssmin
-  //   grunt.initConfig({
-  //      cssmin: {
-  //       target: {
-  //         files: [{
-  //           expand: true,
-  //           cwd: 'views/css',
-  //           src: ['*.css', '!*.min.css'],
-  //           dest: 'views/css',
-  //           ext: '.min.css'
-  //         }]
-  //       }
-  //     }
-  //   });
-
-  // grunt.loadNpmTasks('grunt-contrib-cssmin');
-  // grunt.registerTask('default', ['grunt-contrib-cssmin']);
-
-  //grunt cssnano
-    //require('load-grunt-tasks')(grunt);
     // grunt.initConfig({
     //     cssnano: {
     //         options: {
@@ -74,8 +10,57 @@ module.exports = function(grunt) {
     //                 'views/css/bootstrap-grid.nan.css': 'views/css/bootstrap-grid.css'
     //             }
     //         }
-    //     }
+    //     },
+
+    //     pkg: grunt.file.readJSON('package.json'),
+    //     uglify: {
+    //       options: {
+    //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    //       },
+    //       build: {
+    //         src: 'views/js/main.js',
+    //         dest: 'views/js/main.min.js'
+    //       }
+    //     },
+
+    //     // responsive_image:{
+    //     //   dev: {
+    //     //     options: {
+    //     //       size: [{
+    //     //         quality: 10
+    //     //       }]
+    //     //     }
+    //     //   },
+    //     //   files:[{
+    //     //     expand: true,
+    //     //     src: ['*.{gif,jpg,png}'],
+    //     //     cwd: "views/images/",
+    //     //     dest: 'views/images/reduced/'
+    //     //   }]
+    //     // }
     // });
     // grunt.loadNpmTasks('grunt-cssnano');
-    // grunt.registerTask('default', ['cssnano']);
+    // grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-responsive-images');
+    // grunt.registerTask('default', ['cssnano','uglify']);
+
+    grunt.initConfig({
+        responsive_image:{
+          dev: {
+            options: {
+              size: [{
+                quality: 10
+              }]
+            }
+          },
+          files:[{
+            expand: true,
+            src: ['*.jpg'],
+            cwd: "views/images/",
+            dest: 'views/images/reduced/'
+          }]
+        }
+    });
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.registerTask('default',['responsive_image']);
 };
