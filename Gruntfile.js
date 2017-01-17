38537714fde9a546422c418ba0a530f58b8f6466
MujunZ <o.6180339@gmail.com>
2017-01-17 11:50:16 -0500

    Optimize index.html
    Reduce pictures
    Move CSS to the end
    Add grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,66 +1,49 @@
 module.exports = function(grunt) {
-    // grunt.initConfig({
-    //     cssnano: {
-    //         options: {
-    //             sourcemap: true
-    //         },
-    //         dist: {
-    //             files: {
-    //                 'views/css/style.nan.css': 'views/css/style.css',
-    //                 'views/css/bootstrap-grid.nan.css': 'views/css/bootstrap-grid.css'
-    //             }
-    //         }
-    //     },
-
-    //     pkg: grunt.file.readJSON('package.json'),
-    //     uglify: {
-    //       options: {
-    //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //       },
-    //       build: {
-    //         src: 'views/js/main.js',
-    //         dest: 'views/js/main.min.js'
-    //       }
-    //     },
-
-    //     // responsive_image:{
-    //     //   dev: {
-    //     //     options: {
-    //     //       size: [{
-    //     //         quality: 10
-    //     //       }]
-    //     //     }
-    //     //   },
-    //     //   files:[{
-    //     //     expand: true,
-    //     //     src: ['*.{gif,jpg,png}'],
-    //     //     cwd: "views/images/",
-    //     //     dest: 'views/images/reduced/'
-    //     //   }]
-    //     // }
-    // });
-    // grunt.loadNpmTasks('grunt-cssnano');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-responsive-images');
-    // grunt.registerTask('default', ['cssnano','uglify']);
-
     grunt.initConfig({
-        responsive_image:{
+        cssnano: {
+            options: {
+                sourcemap: true
+            },
+            dist: {
+                files: {
+                    'views/css/style.nan.css': 'views/css/style.css',
+                    'views/css/bootstrap-grid.nan.css': 'views/css/bootstrap-grid.css'
+                }
+            }
+        },
+
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+          },
+          build: {
+            src: 'views/js/main.js',
+            dest: 'views/js/main.min.js'
+          }
+        },
+
+        responsive_images:{
           dev: {
             options: {
-              size: [{
-                quality: 10
+              engine: 'im',
+              sizes: [{
+                name:'small',
+                width: 200,
+                quality: 30,
               }]
-            }
-          },
-          files:[{
+            },
+            files:[{
             expand: true,
-            src: ['*.jpg'],
-            cwd: "views/images/",
-            dest: 'views/images/reduced/'
-          }]
+            src: ['*.{jpg,png}'],
+            cwd: "img/",
+            dest: 'img/reduced/'
+            }]
+          },
         }
     });
+    grunt.loadNpmTasks('grunt-cssnano');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.registerTask('default',['responsive_image']);
+    grunt.registerTask('default', ['cssnano','uglify','responsive_images']);
 };
